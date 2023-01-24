0cc33f8c434537e3dbf32f9b412bbc0df8328544
Will Morcombe <wtmorco@live.co.uk>
2023-01-24 22:44:02 +0000

    Refactored user accounts to new app

---
+++ qudoapp/accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+# Create your models here.
---
+++ qudoapp/accounts/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import get_authorization_header
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserSerializer
+from .models import User
+
+from rest_framework import exceptions
+
+
+class RegisterView(APIView):
+    permission_classes = []
+    def post(self, request):
+
+        serializer = UserSerializer(data = request.data) # send our data to the serializer
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+
+class LoginView(APIView):
+    permission_classes = []
+    def post(self, request):
+
+        # user = User.objects.filter(email=request.data['email']).first()
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(email=email, password=password)
+
+
+
+        if user is not None:
+            data = {
+                'message' : 'Login successful',
+                'token' : user.auth_token.key
+            }
+            return Response(data)
+        else:
+            return Response(data={'message' : 'Invalid credentials'})
+
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # delete auth token to force login
+        request.user.auth_token.delete()
+        return Response({'message' : 'Logout successful'})
