7578a93759f695993282669f03af4ad08adc31c6
Will Morcombe <wtmorco@live.co.uk>
2023-01-26 10:28:14 +0000

    Added models to admin

--- qudoapp/products/admin.py
+++ qudoapp/products/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Orders
+from .models import Orders, OrderItems, Products
 # Register your models here.
 admin.site.register(Orders)
+admin.site.register(OrderItems)
+admin.site.register(Products)
